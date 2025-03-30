573e43a654208ed6871c68ea927b1767833173c2
Filitov Mikhail <filitovme@gmail.com>
2025-03-30 17:19:59 +0200

    better main

--- main.cpp
+++ main.cpp
@@ -2,7 +2,25 @@
 #include <iostream>
 
 int main() {
-    asp::actually_smart_pointer<int> p(new int(5));
+    using asp::actually_smart_pointer;
 
-    std::cout << p.ask("Write a symphony")<< std::endl;
+    std::cout << "Creating smart pointer p...\n";
+    actually_smart_pointer<std::string> p(new std::string("hello"));
+
+    std::cout << "*p = " << *p << "\n";
+
+    {
+        std::cout << "Copying p into q...\n";
+        actually_smart_pointer<std::string> q = p;
+
+        std::cout << "*q = " << *q << "\n";
+
+        std::cout << "Leaving inner scope...\n";
+    }
+
+    std::cout << "Back in outer scope...\n";
+    std::cout << "Now letting p go out of scope...\n";
+
+    return 0;
 }
+
